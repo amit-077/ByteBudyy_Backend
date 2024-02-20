dfb47586306e753566ba818ea5d2a553dc628f44
Amit Kamat <amitkamat918@gmail.com>
2024-02-20 20:16:20 +0530

    Update connectDB.js

--- Database/connectDatabase/connectDB.js
+++ Database/connectDatabase/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connected = await mongoose.connect("mongodb+srv://amitkamat918:WEBSTAr0472@cluster0.fywpj0h.mongodb.net/CodeBytes?retryWrites=true&w=majority");
+    const connected = await mongoose.connect("mongodb+srv://ami_t918:RV9M8boFNG9CU2ik@cluster0.fywpj0h.mongodb.net/?retryWrites=true&w=majority");
 
     if (connected) {
       console.log("Database connected successfully");
